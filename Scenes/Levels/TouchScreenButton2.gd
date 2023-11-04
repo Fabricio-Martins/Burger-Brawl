74330a337cb2ac3cdf73c7f1b2b544a54f009115
Matheus Patriarca <matheussantanapatriarca2019@outlook.com>
2023-11-04 19:09:39 -0300

    feat: adiciona controles mobile

---
+++ Scenes/Levels/TouchScreenButton2.gd
@@ -0,0 +1,8 @@
+extends TouchScreenButton
+
+
+func _ready() -> void:
+	modulate.a8 = 200 
+
+func _process(delta: float) -> void:
+	pass
--- Scenes/Levels/world.gd
+++ Scenes/Levels/world.gd
@@ -4,9 +4,18 @@ var coin_scene = preload("res://Scenes/Scenario/Collectable/coin.tscn")
 var spawn_interval = 1
 var spawn_area = Rect2(Vector2(100, 100), Vector2(400, 400))
 
+@export var _character: Character
+
 func _ready():
 	$Timer.wait_time = spawn_interval
 	$Timer.start()
+	
+	var characters = get_tree().get_nodes_in_group("Character")
+	
+	if characters.size() > 0:
+		_character = characters[0]
+	else:
+		print("Nenhum elemento no grupo 'Character' encontrado.")
 	
 func _process(delta):
 	if Input.is_action_pressed("open_pause"):
@@ -24,3 +33,7 @@ func _on_timer_timeout():
 	add_child(coin_instance)
 	
 	$Timer.start()
+
+
+func _on_touch_screen_button_2_pressed() -> void:
+	_character.manual_dash_enabled = true
