f19289e885cb75eb7d5b082c462ecc195a52a799
Fabricio Flávio <fabriciod@alunos.utfpr.edu.br>
2023-11-06 16:39:18 -0300

    fix: Queue_free() no Pause

--- Scenes/Interface/pause_screen.gd
+++ Scenes/Interface/pause_screen.gd
@@ -9,7 +9,9 @@ func _process(delta):
 func _on_resume_pressed():
 	get_tree().set_pause(false)
 	self.hide()
+	queue_free()
 
 func _on_quit_pressed():
 	get_tree().set_pause(false)
 	get_tree().change_scene_to_file("res://Scenes/Interface/start_screen.tscn")
+	queue_free()
