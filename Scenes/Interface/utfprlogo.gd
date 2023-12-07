ecbc4311d8ff2c7a51341ed20a91525775731389
Matheus Patriarca <matheussantanapatriarca2019@outlook.com>
2023-12-06 21:20:18 -0300

    feat: inclui animação inicial com a logo da utfpr

---
+++ Scenes/Interface/utfprlogo.gd
@@ -0,0 +1,10 @@
+extends Control
+
+
+func _ready():
+	$AnimationPlayer.play("new_animation")
+	await get_tree().create_timer(8).timeout
+	get_tree().change_scene_to_file("res://Scenes/Interface/start_screen.tscn")	
+
+func _process(delta):
+	pass
