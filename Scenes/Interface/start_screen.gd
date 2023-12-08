dc431bf32d79ef7bb978684c83c29b061b474108
Matheus Patriarca <matheussantanapatriarca2019@outlook.com>
2023-12-07 21:15:34 -0300

    feat: adiciona tela de creditos

--- Scenes/Interface/start_screen.gd
+++ Scenes/Interface/start_screen.gd
@@ -16,3 +16,6 @@ func _on_control_btn_pressed():
 
 func _on_control_btn_play():
 	get_tree().change_scene_to_file("res://Scenes/Levels/world.tscn")	
+
+func _on_credits_btn_pressed():
+	get_tree().change_scene_to_file("res://Scenes/Levels/credits_screen.tscn")	
