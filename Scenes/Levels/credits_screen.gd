861e7a42e4bc13dbafb54796c96978c24d298899
Matheus Patriarca <matheussantanapatriarca2019@outlook.com>
2023-12-07 21:53:20 -0300

    feat: adiciona opcao fullscreen telas iniciais

--- Scenes/Levels/credits_screen.gd
+++ Scenes/Levels/credits_screen.gd
@@ -8,18 +8,6 @@ func _ready():
 func _process(delta):
 	pass
 
-func _on_quitBtn_pressed():
-	get_tree().quit()
-
-func _on_control_btn_pressed():
-	get_tree().change_scene_to_file("res://Scenes/Interface/control_screen.tscn")
-
-func _on_control_btn_play():
-	get_tree().change_scene_to_file("res://Scenes/Levels/world.tscn")	
-
-func _on_credits_btn_pressed():
-	get_tree().change_scene_to_file("res://Scenes/Levels/credits_screen.tscn")	
-	
 func _on_pressed() -> void:
 	_toggle_fullscreen()
 
