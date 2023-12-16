c7f162dff2274f474bee2e718f61c98760f8ff3e
Fabricio Flávio <fabriciod@alunos.utfpr.edu.br>
2023-12-16 18:03:34 -0300

    feat: Projétil funcionando

--- Scenes/Enemies/Bread/projectile.gd
+++ Scenes/Enemies/Bread/projectile.gd
@@ -20,4 +20,4 @@ func _physics_process(delta: float) -> void:
 func _on_body_entered(body: Node2D) -> void:
 	if body.has_method("take_damage"):
 		body.take_damage(damage, knockback_force, knockback_direction)
-	#queue_free()
+	queue_free()
