627e5d647a88601d93a47782d1622bb600e22b42
Fabricio Flávio <fabriciod@alunos.utfpr.edu.br>
2023-12-21 00:06:11 -0300

    feat: Inicio dos Weapons

---
+++ Scenes/Weapons/weapon.gd
@@ -0,0 +1,52 @@
+@icon("res://Assets/Weapons/WoodenSpoon.png")
+
+extends Node2D
+class_name Weapon
+
+@onready var animation_player: AnimationPlayer = get_node("WeaponAnimationPlayer")
+@onready var hitbox: Area2D = get_node("Node2D/Texture/Hitbox")
+
+var direction: Vector2 = Vector2.ZERO
+
+@onready var _character: Character
+@onready var player: CharacterBody2D = get_tree().current_scene.get_node("Player")
+@onready var characters = get_tree().get_nodes_in_group("Character")
+var pos_player
+
+func _ready():
+	if characters.size() > 0:
+		_character = characters[0]
+	else:
+		print("Nenhum elemento no grupo 'Character' encontrado.")
+
+func _physics_process(_delta: float) -> void:
+	if is_instance_valid(player):
+		pos_player = _character.get_global_position()
+		
+func get_input() -> void:
+	if OS.has_feature("mobile"):
+		pass
+	else:
+		direction = (get_global_mouse_position() - global_position).normalized()
+		
+	if not (OS.has_feature("mobile")):
+		if Input.is_action_just_pressed("ui_attack") and not animation_player.is_playing() and direction.x > 0:
+			$AttackAudio.play()
+			animation_player.play("attack_right")
+		elif Input.is_action_just_pressed("ui_attack") and not animation_player.is_playing() and direction.x < 0:
+			$AttackAudio.play()
+			animation_player.play("attack_left")
+
+func move(mouse_direction: Vector2) -> void:
+	rotation = mouse_direction.angle()
+	hitbox.knockback_direction = mouse_direction
+	
+	if mouse_direction.x > 0:
+		scale.y = 1
+	else:
+		scale.y = -1
+
+func is_busy() -> bool:
+	if animation_player.is_playing():
+		return true
+	return false
